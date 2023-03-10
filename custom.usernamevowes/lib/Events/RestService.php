7643492fd3b9d2a99ab91b0dacef7fa1d57c561e
Lidhen <lidhen@yandex.ru>
2023-03-10 14:03:59 +0300

    RestService

--- custom.usernamevowes/lib/Events/RestService.php
+++ custom.usernamevowes/lib/Events/RestService.php
@@ -5,29 +5,29 @@ use Bitrix\Main\SystemException;
 
 class RestService
 {
-    public static function getDescription()
+	public static function getDescription()
 	{
 		return array(
 			'vowes' => array(
 				'vowes.get.usernamevovwes' =>  array(__CLASS__, 'GetUserNameVowes'),
-	    	)
+			)
 		);
 	}
-    public static function GetUserNameVowes($query, $n, \CRestServer $server)
+	public static function GetUserNameVowes($query, $n, \CRestServer $server)
 	{
 		try
-        {
+		{
 			$UserId = null;
 			if($query['user_id']){
 				$UserId = intval($query['user_id']);
 			}			
-            $name = (new \Custom\UserNameVowes\PrepareUserName())->getUserNameVowels($UserId);
-            return ['status' => 'success' , 'result' => $name];
-        }
-        catch (SystemException $exception)
-        {
-            return ['status' => 'error' , 'error_msg' => $exception->getMessage()];
-        }
-		return $result;
+			$name = (new \Custom\UserNameVowes\PrepareUserName())->getUserNameVowels($UserId);
+
+			return ['status' => 'success' , 'result' => $name];
+		}
+		catch (SystemException $exception)
+		{
+			return ['status' => 'error' , 'error_msg' => $exception->getMessage()];
+		}
 	}
 }
